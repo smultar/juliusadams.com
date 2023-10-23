8fab86d1f433d9169e18444c916311ffb5e200ee
Jason <Jason@smultar.com>
2023-10-23 10:12:53 -0500

    Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
-import Menu from '@/components/menu';
 import './layout.scss';
+import Menu from '@/components/menu';
 
 import Providers from '@/components/providers';
 import Footer from '@/components/footer';
 
+import type { Metadata } from 'next';
+
 type LayoutProps = {
 	children: React.ReactNode;
+};
+
+const metadata: Metadata = {
+	title: 'Julius Adams',
+	description: 'Julius Adams is a musical artist based in the United States.',
 };
 
 const Layout = ({ children }: LayoutProps) => {
@@ -14,6 +21,7 @@ const Layout = ({ children }: LayoutProps) => {
 
 	return (
 		<html>
+			<link rel='icon' href='/svgs/music.svg' sizes='any' />
 			<body>
 				<Providers>
 					<Menu />
@@ -26,3 +34,4 @@ const Layout = ({ children }: LayoutProps) => {
 };
 
 export default Layout;
+export { metadata };
