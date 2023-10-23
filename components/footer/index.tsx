6affcb9ca6361e331ccd06d007e834b2c2d21537
Jason <Jason@smultar.com>
2023-10-23 10:06:45 -0500

    Updated all footer links

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -8,19 +8,22 @@ const Footer = () => {
 			<div className='footer-content'>
 				<p>Julius Adams © 2023</p>
 				<div className='links'>
-					<Link href='/'>
+					<Link href='https://open.spotify.com/artist/0j1rkk9rCD6ak61ZCgeVVY' target='_blank' passHref>
 						<Image className='link' src='/svgs/spotify.svg' width={18} height={18} alt='Music Logo' />
 					</Link>
-					<Link href='/'>
+					<Link href='https://music.apple.com/us/artist/julius-adams/251451739' target='_blank' passHref>
 						<Image className='link' src='/svgs/itunes.svg' width={18} height={18} alt='Music Logo' />
 					</Link>
-					<Link href='/'>
+					<Link href='https://www.facebook.com/SingerJuliusAdams' target='_blank' passHref>
 						<Image className='link' src='/svgs/facebook.svg' width={18} height={18} alt='Music Logo' />
 					</Link>
-					<Link href='/'>
+					<Link href='https://www.tiktok.com/@julius_adams' target='_blank' passHref>
+						<Image className='link' src='/svgs/tiktok.svg' width={18} height={18} alt='Music Logo' />
+					</Link>
+					<Link href='https://x.com/juliusadams14' target='_blank' passHref>
 						<Image className='link' src='/svgs/twitter.svg' width={18} height={18} alt='Music Logo' />
 					</Link>
-					<Link href='/'>
+					<Link href='https://www.instagram.com/juliusvadams' target='_blank' passHref>
 						<Image className='link' src='/svgs/instagram.svg' width={18} height={18} alt='Music Logo' />
 					</Link>
 				</div>
